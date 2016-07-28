5913b6b64befcdd421610b3ec4796a5d43e56b98
Jim Lin <sdf611097@gmail.com>
2016-07-28 17:27:51 +0800

    Using port depends on env var PORT

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.post('/add', function(req, res){
     }
 });
 
-app.listen(3000, function () {
-      console.log('Example app listening on port 3000!');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+      console.log('Example app listening on port ' + port);
 });
