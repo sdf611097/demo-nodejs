98c275292251d568223a42b971140dec2f8205c6
Jim Lin <sdf611097@gmail.com>
2016-07-28 17:01:06 +0800

    Api server support add function

--- function.js
+++ function.js
@@ -16,12 +16,6 @@ var divide = (arg1, arg2) => {
     return arg1 / arg2;
 }
 
-console.log('add', 1, 2,'=',  add(1,2));
-console.log('del', 1, 2,'=',  del(1,2));
-console.log('mul', 1, 2,'=',  multiply(1,2));
-console.log('div', 1, 2,'=',  divide(1,2));
-
-
 module.exports = {
     add,
     del : del,
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+var func = require('./function.js');
+
+app.use(bodyParser.json({type: '*/*'}));
 
 app.get('/', function (req, res) {
       res.send('Ni Hao');
+});
+
+app.post('/add', function(req, res){
+    console.log(req.body);
+    let a = req.body.a;
+    let b = req.body.b;
+    if(a<0){
+        res.status(400).send('a<0');
+    }else{
+        res.send('' + func.add(a,b));
+    }
 });
 
 app.listen(3000, function () {
