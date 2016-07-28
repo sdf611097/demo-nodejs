018980dfd23ac5351bc8b49cb8d86a5f39a09e8b
Jim Lin <sdf611097@gmail.com>
2016-07-28 16:16:53 +0800

    javscript function

--- function.js
+++ function.js
@@ -1,1 +1,25 @@
-function 
+'use strict';
+
+function add(arg1, arg2){
+    return arg1 + arg2;   
+}
+
+var del;
+
+del = function(arg1, arg2){
+    return arg1 - arg2;
+}
+
+var multiply = (arg1, arg2) => arg1 * arg2 ;
+
+var divide = (arg1, arg2) => {
+    return arg1 / arg2;
+}
+
+console.log('add', 1, 2,'=',  add(1,2));
+console.log('del', 1, 2,'=',  del(1,2));
+console.log('mul', 1, 2,'=',  multiply(1,2));
+console.log('div', 1, 2,'=',  divide(1,2));
+
+
+
